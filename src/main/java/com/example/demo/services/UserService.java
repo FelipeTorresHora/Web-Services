b6296cf7ef4b5d463bc6fe5a060a202cf394d65c
Felipe <fhora93@gmail.com>
2024-06-18 22:39:18 -0300

    Service Layer

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+
+    }
+}
